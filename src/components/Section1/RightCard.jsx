fc842ad3685bcc8af74a0383efdda72025f7470f
VivekMore1011 <vivekmore1011@gmail.com>
2026-09-06 12:45:34 +0530

    UI Project

---
+++ src/components/Section1/RightCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const RightCard = () => {
+  return (
+    <div className='h-full w-80  rounded-4xl overflow-hidden'>
+
+        <img src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""  className='h-full w-full object-cover'/>
+<div className='absolute top-0 left-0 he-full w-full bg-amber-300'>
+
+
+</div>
+    </div>
+  )
+}
+
+export default RightCard
---
+++ src/components/Section1/RightContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import RightCard from './RightCard'
+
+
+const RightContent = () => {
+    return (
+        <div className='p-6 h-full w-2/3 bg-blue-400 text'>
+
+            <RightCard/>
+
+</div>
+    )
+}
+
+export default RightContent
